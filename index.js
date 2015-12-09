b020194366d3ee5d8c446177cad769798bb018c4
Alexey Bondarenko <alexeybondarenko@me.com>
2015-12-09 22:57:25 +0200

    added parsing poems

--- index.js
+++ index.js
@@ -1,36 +1,111 @@
 var request = require("request-promise"),
-    cheerio = require("cheerio");
+    cheerio = require("cheerio"),
+    Q = require("bluebird");
+    async = require("async");
 
-var authorsPageUrl = 'http://onlyart.org.ua/?page_id=5026';
-function getParamFromUrlQuery ( name, url) {
+function getParamFromUrlQuery(name, url) {
     if (!url) url = location.href;
-    name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var regexS = "[\\?&]"+name+"=([^&#]*)";
-    var regex = new RegExp( regexS );
-    var results = regex.exec( url );
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(url);
     return results == null ? null : results[1];
 }
 
-function getAuthorsPages () {
-    request({
-        uri: authorsPageUrl,
+function req(url) {
+    return request({
+        uri: url,
         transform: function (body) {
             return cheerio.load(body);
         }
-    }).then(function ($) {
-        var result = [];
-        $('#posts li').each(function (i, el) {
-            var link = $('a', el),
-                href = link.attr('href');
-
-            if (!href) return;
-            result.push({
-                id: getParamFromUrlQuery('page_id', href),
-                name: link.text()
-            });
-        });
-        console.log(result);
     });
 }
 
-getAuthorsPages();
+function getAuthorsPages(autorListPage) {
+    return req(autorListPage).then(function ($) {
+        var result = [];
+        $('#posts li a').each(function (i, el) {
+            var elem = $(el);
+            var href = elem.attr('href');
+
+            if (!href) return;
+            result.push({
+                id: parseInt(getParamFromUrlQuery('page_id', href)),
+                name: elem.text(),
+                url: href
+            });
+        });
+        return result;
+    });
+}
+function getPoemsPages(autorPageId) {
+    return req('http://onlyart.org.ua/?page_id=' + autorPageId).then(function ($) {
+        var result = [];
+        $('#post-' + autorPageId + ' ul li a').each(function (i, el) {
+            var elem = $(el);
+            var href = elem.attr('href');
+            if (!href) return;
+            result.push({
+                id: parseInt(getParamFromUrlQuery('page_id', href)),
+                name: elem.text(),
+                url: href
+            });
+        });
+        return result;
+    })
+}
+function getPoemPage (poemPageId) {
+    return req('http://onlyart.org.ua/?page_id=' + poemPageId).then(function ($) {
+        var result = [];
+        $('#post-' + poemPageId + ' .entry p').each(function (i, el) {
+            var elem = $(el);
+            result.push(elem.text());
+        });
+        return result;
+    })
+}
+
+var authorsListPageUrl = 'http://onlyart.org.ua/?page_id=5026';
+//getAuthorsPages(authorsListPageUrl).then(function (authors) {
+//    console.log(authors);
+//    var promises = authors.map(function (autorItem) {
+//        return getPoemPages(autorItem.id).catch(function (resp) {
+//            return null;
+//        });
+//    });
+//    return Q.all(promises);
+//
+//});
+var testAuthor = {
+    id: 11567,
+    name: 'Герасим’юк Василь',
+    url: 'http://onlyart.org.ua/?page_id=11567'
+};
+var current = Q.resolve();
+getPoemsPages(testAuthor.id).then(function (resp) {
+    return Q.some(resp.map(function(item) {
+        current = current.then(function () {
+            return getPoemPage(item.id);
+        }).then(function(result) {
+            return {
+                name: item.name,
+                content: result
+            };
+        });
+        return current;
+    }), 10);
+
+}).then(function (resp) {
+    console.log(resp);
+});
+
+//var testPoem =  {
+//    id: 11831,
+//    name: '“На сіні, що срібліє над кущами…”',
+//    url: 'http://onlyart.org.ua/?page_id=11831'
+//};
+//
+//
+//getPoemPage (testPoem.id).then(function (resp) {
+//    console.log(resp);
+//});
